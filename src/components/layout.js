5294cce9ed9686b1a0ea7be35d047d725815c78b
Haddee Ann <haddeeann@gmail.com>
2021-12-11 04:37:23 -0500

    some green sock animation

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
-import * as React from "react"
+import React, { useRef, useEffect } from "react"
 import { Link } from "gatsby"
 import { StaticImage } from 'gatsby-plugin-image'
-
+import { gsap } from 'gsap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
@@ -9,18 +9,35 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons'
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
+  const titleRef = useRef()
+  const logoRef = useRef()
+
+  useEffect(() => {
+    gsap.from(titleRef.current, {
+      color: 'rgba(178, 205, 255, 1)',
+      opacity: 0,
+      duration: 5
+    })
+
+    gsap.from(logoRef.current, {
+      opacity: 0,
+      duration: 5
+    })
+  })
 
   return (
     <div>
       <div className='header'>
         <header className="global-header">
-          <StaticImage 
-            alt="Yellow Umbrella Makers Logo"
-            src="../images/umbrellaLogoSMALL.png"
-          />
+          <span ref={logoRef}>
+            <StaticImage 
+              alt="Yellow Umbrella Makers Logo"
+              src="../images/umbrellaLogoSMALL.png"
+            />
+          </span>
           <div className='heading'>
             <h1 className="main-heading">
-              <Link to="/">{title}</Link>
+              <Link to="/" ref={titleRef}>{title}</Link>
             </h1>
             <nav>
               <ul>
